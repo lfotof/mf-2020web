89059c8b238921605e65c8255888876f25f6042b
Minfat <2621607484@qq.com>
2021-01-13 17:35:45 +0800

    变量提升

--- 变量提升&LET&闭包作用域/1.变量提升.js
+++ 变量提升&LET&闭包作用域/1.变量提升.js
@@ -25,8 +25,164 @@ function sum(n,m) {//5.执行到这里直接跳过
 
 
 
-//函数表达式
-console.log(sum1(10,20))//Uncaught TypeError: sum1 is not a function 直接报错来，var声明到只能是变量提升，并不会像函数声明那样进行定义所以报错。
+/*函数表达式
+由于使用var只存在变量提升不会赋值，所以此时函数在前面执行，函数是没有值的，不能执行(真实项目中使用这种方法，因为他操作更加严谨)
+*/
+console.log(sum1)//undefined
+sum1(10,20)//Uncaught TypeError: sum1 is not a function 直接报错来，var声明到只能是变量提升，并不会像函数声明那样进行定义所以报错。
 var sum1 =function(n,m) {
     return n+m
 }
+
+
+
+/*
+练习题1
+ 全局作用域(栈内存)
+ 1.变量提升 var a; 值为空
+ 2.代码执行
+*/
+console.log(c)//undefined
+var c = 12 //var c不执行 在变量提升到时候已经声明，直接将12与c关联 c的值12
+c = 13 //重新赋值，将13与c关联 c到值为13
+console.log(c)
+
+
+/*
+练习题2
+词法解析直接报错来，语法有错误。
+只有var和function存在变量提升
+在JS中上一行代码报错，下面都不会再去执行
+1.变量提升 没有var和function不存在变量提升
+2.代码执行
+*/
+console.log(d)
+let d = 12 
+d = 13 
+console.log(d)
+
+
+/*
+练习题3
+1. 变量提升 没有var和function不存在变量提升
+2.代码执行
+*/
+console.log(f)//词法解析直接报错来，语法有错误
+f=13 //window.f=13
+console.log(f)
+
+/*
+练习题4
+1. 变量提升 没有var和function不存在变量提升
+2.代码执行
+*/
+let h =12 
+let h = 13 //报错 语法错误 重复声明 let不能在同一作用域下重复声明 var允许
+console.log(h)
+
+
+/*
+练习题5
+词法解析的时候直接报错了，因为下面的语法就错误了。词法解析也可以叫词法检测 语法错误
+*/
+console.log(1)
+let j =12 
+console.log(j)
+let j = 13 
+console.log(j)
+
+/*
+练习题6
+SyntaxError
+所谓重复是：不管之前通过什么方法，只要在当前栈中存在了这个变量，我们使用let/const等重复再声明这个变量就是语法错误
+*/
+console.log(k)
+var k =12 
+let k = 13//报错  语法错误
+console.log(k)
+
+/*
+练习题7
+*/
+fn()
+function fn() {console.log(1)}
+fn()
+function fn() {console.log(2)}
+fn()
+var fn = function() {console.log(3)}
+fn()
+function fn(){console.log(4)}
+fn()
+function fn(){console.log(5)}
+fn()
+
+
+/*
+关于条件判断的
+[PROPERTY] in [OBJECT] 验证当前属性是否存在于对象
+hansOwnProperty 集成于
+*/
+let obj = {
+    name:'杨点点',
+    age:3,
+    GF:null
+}
+console.log("name" in obj)//true
+console.log("BF" in obj)//false
+/*
+练习题8
+    1.变量提升
+    不管条件是否成立都要进行变量提升
+    var a; //创建一个全局变量a 也相当于给window添加一个属性
+*/
+console.log(a)//undefined
+if(!('a' in window)) {//'a' in window 为true !('a' in window)为false
+    var a = 13 //不走这里了。
+}
+console.log(a)//undefined
+
+/*
+练习题9
+全局作用域
+1.变量提升
+函数有特殊性：在老版本游览器中，确实不论条件是否成立，都会进行变量提升和定义，但是在新版本游览器中为了兼容ES6严谨的语法规范，条件中的函数只会提升不会定义。
+*/
+console.log(fn);//undefined
+fn()//Uncaught typeError:fn is ont a function
+if('fn' in  window) {//true
+    // 条件成立，进来后的第一件事是给 FN赋值
+    fn()
+    function fn() {
+        console.log('哈哈哈')
+    }
+}
+fn()
+
+/*
+自执行函数：前面加的()或者!、-、~、+只有一个目的，让语法符合而已
+自执行函数本身不进行变量提升（没名字）
+(function(n){})(10)
+~function(n){}(10)
+!function(n){}(10)
++function(n){}(10)
+-function(n){}(10)
+*/
+
+/*
+练习题10
+*/
+f= function () {return true}
+g= function () {return false}
+~function() {
+    /*
+    函数执行会形成一个私有作用域
+    1.变量提升 function g
+    2.代码执行
+    */
+   if(g() && []==![]) {//Uncaught typeError:g is ont a function
+       f=function() {return false}
+       function g() {return true}
+   }
+}()
+console.log(f())
+console.log(g())
--- 变量提升&LET&闭包作用域/README.md
+++ 变量提升&LET&闭包作用域/README.md
@@ -2,4 +2,34 @@
 当游览器开辟出供代码执行的栈内存后，代码并没有自上而下执行，而是继续做了一些事情：
 把当前作用域中所有带var和function的关键字进行提前声明和定义=>变量提升
 *带var的知识提前声明[declare](var a;)，如果只声明值为undefined
+**1.1带var和不带var的区别 
+在全局作用域下带var是变量，不带var的是给window加了一个属性
+var b = 14 //变量 在全局下声明的变量也相当于在window添加了一个对应的属性
+console.log(b)//14
+console.log(window.b)//14//window.a =14 window的一个属性
 *带function的不仅声明，而且还定义了(defined定义) "a=13"定义就是赋值，准确来说就是让变了和每个值进行关联
+
+
+
+
+2.let/const 和var到区别
+
+2.1 1et/const 不存在变了提升。创建变了的6种方式种只有var和function有变量提升，而let/const/import/class都不存在这个机制。
+
+2.2在相同的作用域中or执行上下文中,var/function允许重复声明，声明第一次之后，之后再遇到就不会在重复声明了，而let/const是不允许的。
+//在游览器自上而下执行之前，在执行之前还有其他操作，比如词法解析它会检测即将执行的代码是否存在语法错误，存在就报错SyntaxError，所以下面的案例直接报错了，代码不会执行。
+console.log(1)//不执行
+let j =12 
+console.log(j)
+let j = 13 
+console.log(j)
+
+2.3 let能解决typeof检测时存在的暂时性死区
+console.log(a) //报错a未定义
+console.log(typeof a) //undefined 这是游览器的BUG，本应该是报错的，因为没有a(暂时性死区)
+
+console.log(typeof a)//a未初始化
+let a 
+
+
+
